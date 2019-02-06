844531f5859b5c52bfbc230e667b07fff0c6f1b4
Thibaut Lacroix <thibautlac@gmail.com>
2019-02-06 11:12:55 +0100

    :wrench: Update Patternlab structure

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,5 +1,5 @@
 "use strict";
-console.log('Beginning Pattern Lab Node Gulp postinstall...');
+console.log("Beginning Pattern Lab Node Gulp postinstall...");
 
 //call the core library postinstall
-var patternlab = require('patternlab-node/core/scripts/postinstall');
+var patternlab = require("patternlab-node/core/scripts/postinstall");
